19a652ce9f2222c050e3f2bbae15c0ad42a741df
FireIsGood <fireisadequate@gmail.com>
2025-10-26 02:56:09 -0700

    fix: confetti multiplier not being saved

--- packages/frontend/src/app/services/settings.service.ts
+++ packages/frontend/src/app/services/settings.service.ts
@@ -562,6 +562,7 @@ export class SettingsService {
       translationKey: 'settings.confettiMultiplier',
       translationDescriptionKey: 'settings.confettiMultiplierDescription',
       serverKey: 'wafrn.confettiMultiplier',
+      localStorageKey: 'confettiMultiplier',
       type: 'input',
       default: '1'
     },
