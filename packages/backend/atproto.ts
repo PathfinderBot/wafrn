89d70d6dfe175277fd3fc5b0fb97b4b5269cc5af
jbcarreon123 <jbcarreon212@gmail.com>
2025-10-31 15:35:36 +0800

    bite from bluesky v1

--- packages/backend/atproto.ts
+++ packages/backend/atproto.ts
@@ -1,4 +1,5 @@
 import { Firehose } from '@skyware/firehose'
+import { Jetstream } from '@skyware/jetstream'
 import { getCacheAtDids } from './atproto/cache/getCacheAtDids.js'
 import { Job, Queue, Worker } from 'bullmq'
 import { checkCommitMentions } from './atproto/utils/checkCommitMentions.js'
@@ -8,8 +9,12 @@ import { completeEnvironment } from './utils/backendOptions.js'
 //const firehose = new Firehose(`wss://bolson.bsky.dev`);
 
 let cachedDids = await getCacheAtDids(true)
-const firehose = new Firehose({
-  relay: `wss://atproto.africa`
+// const firehose = new Firehose({
+//   relay: `wss://atproto.africa`
+// })
+
+const jetstream = new Jetstream({
+  endpoint: 'wss://jetstream.sprk.so/subscribe'
 })
 
 const firehoseQueue = new Queue('firehoseQueue', {
@@ -25,19 +30,23 @@ const firehoseQueue = new Queue('firehoseQueue', {
   }
 })
 
-firehose.on('commit', async (commit) => {
+jetstream.on('commit', async (event) => {
   const cacheData = cachedDids
+  const commit = event.commit
 
-  if (cacheData.followedDids.has(commit.repo) || checkCommitMentions(commit, cacheData))
-    for await (const op of commit.ops) {
-      const data = {
-        repo: commit.repo,
-        operation: op
+  if (cacheData.followedDids.has(event.did) || checkCommitMentions(event.did, commit, cacheData) || commit.collection === 'net.wafrn.feed.bite') {
+    const data = {
+      repo: event.did,
+      operation: {
+        ...(commit as any).record,
+        action: commit.operation,
+        path: `${commit.collection}/${commit.rkey}`
       }
-      await firehoseQueue.add('processFirehoseQueue', data)
     }
+    await firehoseQueue.add('processFirehoseQueue', data)
+  }
 })
-firehose.start()
+jetstream.start()
 
 const workerForceUpdateAtDidCache = new Worker(
   'forceUpdateDids',
