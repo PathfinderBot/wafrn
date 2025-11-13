3a7bd4bfb89276dfd842dfefa13a8779bfa3d2bc
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-11-13 09:55:29 +0100

    try this one jb

--- packages/backend/migrations/2025.11.12T14.48.02.bsky-indexes.ts
+++ packages/backend/migrations/2025.11.12T14.48.02.bsky-indexes.ts
@@ -7,11 +7,16 @@ import { DataType } from 'sequelize-typescript'
 export const up: Migration = async (params) => {
   const queryInterface = params.context
 
+    await queryInterface.sequelize.query(`CREATE INDEX IF NOT EXISTS post_bsky_uri_tmp  ON "posts" ("bskyUri");`);
+
   await queryInterface.sequelize.query(`UPDATE "posts" SET "bskyUri" = NULL WHERE "id" IN (select "id" from "posts" ou
 where "bskyUri" IS NOT NULL and (select count(*) from "posts" inr
 where inr."bskyUri" IS NOT NULL AND ou."bskyUri" IS NOT NULL AND inr."bskyUri" = ou."bskyUri") > 1);`);
 
   await queryInterface.sequelize.query(`CREATE UNIQUE INDEX IF NOT EXISTS post_bsky_uri  ON "posts" ("bskyUri");`);
+
+    await queryInterface.sequelize.query(`DROP INDEX post_bsky_uri_tmp;`)
+
 
 }
 export const down: Migration = async (params) => {
