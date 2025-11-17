dc0502052cfdb5f9cf01df31ff7109d74e35a637
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-11-17 21:31:41 +0100

    linting

--- packages/backend/utils/cacheGetters/getPostAndUserFromPostId.ts
+++ packages/backend/utils/cacheGetters/getPostAndUserFromPostId.ts
@@ -1,135 +1,153 @@
-import { Op } from 'sequelize'
-import { Ask, Emoji, EmojiReaction, Media, Post, PostTag, User, UserLikesPostRelations } from '../../models/index.js'
-import { redisCache } from '../redis.js'
-import { Privacy } from '../../models/post.js'
+import { Op } from "sequelize";
+import {
+  Ask,
+  Emoji,
+  EmojiReaction,
+  Media,
+  Post,
+  PostTag,
+  User,
+  UserLikesPostRelations,
+} from "../../models/index.js";
+import { redisCache } from "../redis.js";
+import { Privacy } from "../../models/post.js";
 
-async function getPostAndUserFromPostId(postId: string): Promise<{ found: boolean; data?: any }> {
-  const cacheResult = await redisCache.get('postAndUser:' + postId)
-  let res: { found: boolean; data?: any } = cacheResult ? JSON.parse(cacheResult) : { found: false }
+async function getPostAndUserFromPostId(
+  postId: string
+): Promise<{ found: boolean; data?: any }> {
+  const cacheResult = await redisCache.get("postAndUser:" + postId);
+  let res: { found: boolean; data?: any } = cacheResult
+    ? JSON.parse(cacheResult)
+    : { found: false };
   if (!cacheResult) {
     const dbQuery = await Post.findOne({
       include: [
         {
-          model: Ask
+          model: Ask,
         },
         {
           model: User,
-          as: 'user',
+          as: "user",
           required: true,
           where: {
-            banned: false
-          }
+            banned: false,
+          },
         },
         {
           model: Post,
           include: [
             {
               model: User,
-              as: 'user'
-            }
+              as: "user",
+            },
           ],
-          as: 'quoted',
+          as: "quoted",
           where: {
-            isDeleted: false
+            isDeleted: false,
           },
-          required: false
+          required: false,
         },
         {
           model: Post,
-          as: 'parent',
+          as: "parent",
           required: false,
           where: {
-            isDeleted: false
+            isDeleted: false,
           },
           include: [
             {
               model: Media,
-              required: false
+              required: false,
             },
             {
               model: PostTag,
-              required: false
-            }
-          ]
+              required: false,
+            },
+          ],
         },
         {
           model: User,
-          as: 'mentionPost',
-          required: false
+          as: "mentionPost",
+          required: false,
         },
         {
           model: Media,
-          required: false
+          required: false,
         },
         {
           model: PostTag,
-          required: false
+          required: false,
         },
         {
           model: Emoji,
-          required: false
-        }
+          required: false,
+        },
       ],
       where: {
         id: postId,
         isDeleted: false,
         privacy: {
-          [Op.notIn]: [Privacy.LocalOnly]
-        }
-      }
-    })
+          [Op.notIn]: [Privacy.LocalOnly],
+        },
+      },
+    });
     if (dbQuery) {
       // check if its a bsky post because we dont enjoy bsky posts going to fedi!
       const parents = await dbQuery.getAncestors({
         include: [
           {
             model: User,
-            as: 'user'
-          }
-        ]
-      })
-      const isBskyPost = parents.some((elem) => elem.isRemoteBlueskyPost)
+            as: "user",
+          },
+        ],
+      });
+      const isBskyPost = parents.some((elem) => elem.isRemoteBlueskyPost);
       if (isBskyPost) {
-        res = { found: false }
-        return res
+        res = { found: false };
+        return res;
       }
 
       let likes = UserLikesPostRelations.findAll({
         where: {
-          postId: postId
-        }
-      })
+          postId: postId,
+        },
+      });
       let shares = Post.findAll({
         where: {
           parentId: postId,
-          isReblog: true
-        }
-      })
+          isReblog: true,
+        },
+      });
       let reacts = EmojiReaction.findAll({
         where: {
-          postId: postId
-        }
-      })
-      Promise.all([likes, shares, reacts])
+          postId: postId,
+        },
+      });
+      Promise.all([likes, shares, reacts]);
 
-      res = { found: true, data: dbQuery.dataValues }
+      res = { found: true, data: dbQuery.dataValues };
       if (res.data.ask) {
-        const userAsker = await User.findByPk(res.data.ask.userAsker)
-        res.data.ask.asker = userAsker
+        const userAsker = await User.findByPk(res.data.ask.userAsker);
+        res.data.ask.asker = userAsker;
       }
-      res.data.shares = await shares
-      res.data.likes = await likes
-      res.data.reacts = await reacts
+      res.data.shares = await shares;
+      res.data.likes = await likes;
+      res.data.reacts = await reacts;
     } else {
-      res = { found: false }
+      res = { found: false };
     }
     if (res.found) {
-      await redisCache.set('postAndUser:' + postId, JSON.stringify(res), 'EX', 300)
+      await redisCache.set(
+        "postAndUser:" + postId,
+        JSON.stringify(res),
+        "EX",
+        300
+      );
     } else {
       // await redisCache.set('postAndUser:' + postId, JSON.stringify(res), 'EX', 60)
     }
   }
-  return res
+  return res;
 }
 
-export { getPostAndUserFromPostId }
+export { getPostAndUserFromPostId };
