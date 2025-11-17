d6c6717179d28223017854e899cf3926e88a1bdc
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-11-17 21:54:47 +0100

    if conection gets closed we start it again

--- packages/backend/atproto.ts
+++ packages/backend/atproto.ts
@@ -1,82 +1,93 @@
-import { Firehose } from '@skyware/firehose'
-import { Jetstream } from '@skyware/jetstream'
-import { getCacheAtDids } from './atproto/cache/getCacheAtDids.js'
-import { Job, Queue, Worker } from 'bullmq'
-import { checkCommitMentions } from './atproto/utils/checkCommitMentions.js'
-import { logger } from './utils/logger.js'
-import { completeEnvironment } from './utils/backendOptions.js'
+import { Firehose } from "@skyware/firehose";
+import { Jetstream } from "@skyware/jetstream";
+import { getCacheAtDids } from "./atproto/cache/getCacheAtDids.js";
+import { Job, Queue, Worker } from "bullmq";
+import { checkCommitMentions } from "./atproto/utils/checkCommitMentions.js";
+import { logger } from "./utils/logger.js";
+import { completeEnvironment } from "./utils/backendOptions.js";
 
 //const firehose = new Firehose(`wss://bolson.bsky.dev`);
 
-let cachedDids = await getCacheAtDids(true)
+let cachedDids = await getCacheAtDids(true);
 // const firehose = new Firehose({
 //   relay: `wss://atproto.africa`
 // })
 
 const jetstream = new Jetstream({
-  endpoint: 'wss://jetstream.fire.hose.cam/subscribe',
+  endpoint: "wss://jetstream.fire.hose.cam/subscribe",
   wantedCollections: [
-    'net.wafrn.feed.bite',
-    'app.bsky.feed.like',
-    'app.bsky.feed.post',
-    'app.bsky.feed.repost',
-    'app.bsky.graph.follow',
-    'app.bsky.graph.block',
-    'app.bsky.feed.threadgate'
-  ]
-})
+    "net.wafrn.feed.bite",
+    "app.bsky.feed.like",
+    "app.bsky.feed.post",
+    "app.bsky.feed.repost",
+    "app.bsky.graph.follow",
+    "app.bsky.graph.block",
+    "app.bsky.feed.threadgate",
+  ],
+});
 
-const firehoseQueue = new Queue('firehoseQueue', {
+const firehoseQueue = new Queue("firehoseQueue", {
   connection: completeEnvironment.bullmqConnection,
   defaultJobOptions: {
     removeOnComplete: true,
     attempts: 6,
     backoff: {
-      type: 'exponential',
-      delay: 25000
+      type: "exponential",
+      delay: 25000,
     },
-    removeOnFail: false
-  }
-})
+    removeOnFail: false,
+  },
+});
 
-jetstream.on('commit', async (event) => {
-  const cacheData = cachedDids
-  const commit = event.commit
+jetstream.on("commit", async (event) => {
+  const cacheData = cachedDids;
+  const commit = event.commit;
 
-  if (cacheData.followedDids.has(event.did) || checkCommitMentions(event.did, commit, cacheData) || commit.collection === 'net.wafrn.feed.bite') {
+  if (
+    cacheData.followedDids.has(event.did) ||
+    checkCommitMentions(event.did, commit, cacheData) ||
+    commit.collection === "net.wafrn.feed.bite"
+  ) {
+    logger.debug("obtaining");
     const data = {
       repo: event.did,
       operation: {
         ...(commit as any),
         action: commit.operation,
         collection: commit.collection,
-        path: `${commit.collection}/${commit.rkey}`
-      }
-    }
-    await firehoseQueue.add('processFirehoseQueue', data)
+        path: `${commit.collection}/${commit.rkey}`,
+      },
+    };
+    await firehoseQueue.add("processFirehoseQueue", data);
   }
-})
-jetstream.start()
+});
+
+jetstream.on("close", () => {
+  logger.warn("jetstream closed");
+  jetstream.start();
+});
+
+jetstream.start();
 
 const workerForceUpdateAtDidCache = new Worker(
-  'forceUpdateDids',
+  "forceUpdateDids",
   async (job: Job) => {
-    logger.info(`Atproto force update of dids`)
-    const tmp = await getCacheAtDids(true)
-    cachedDids = tmp
+    logger.info(`Atproto force update of dids`);
+    const tmp = await getCacheAtDids(true);
+    cachedDids = tmp;
   },
   {
     connection: completeEnvironment.bullmqConnection,
     concurrency: 1,
-    lockDuration: 120000
+    lockDuration: 120000,
   }
-)
+);
 
-workerForceUpdateAtDidCache.on('failed', (err) => {
+workerForceUpdateAtDidCache.on("failed", (err) => {
   logger.warn({
     message: `workerforceUpdateDids failed`,
-    error: err
-  })
-})
+    error: err,
+  });
+});
 
-logger.info('started atproto')
+logger.info("started atproto");
