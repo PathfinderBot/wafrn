f9d4b89b32c3d7b87035f131a81453f6c1e9ffe1
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-11-20 11:35:18 +0100

    debug

--- packages/backend/utils/queueProcessors/sendPostToInboxes.ts
+++ packages/backend/utils/queueProcessors/sendPostToInboxes.ts
@@ -1,14 +1,15 @@
-import { Job } from 'bullmq'
-import { logger } from '../logger.js'
-import { postPetitionSigned } from '../activitypub/postPetitionSigned.js'
+import { Job } from "bullmq";
+import { logger } from "../logger.js";
+import { postPetitionSigned } from "../activitypub/postPetitionSigned.js";
 
 async function sendPostToInboxes(job: Job) {
-  const inbox: string = job.data.inboxList
-  const localUser = job.data.petitionBy
-  const objectToSend = job.data.objectToSend
+  const inbox: string = job.data.inboxList;
+  const localUser = job.data.petitionBy;
+  const objectToSend = job.data.objectToSend;
   //at some point we should remove the array thing but at the same time yeah
-  const tmp = await postPetitionSigned(objectToSend, localUser, inbox)
-  return true
+  logger.debug({ message: "sendpostToInbox", inbox, localUser });
+  const tmp = await postPetitionSigned(objectToSend, localUser, inbox);
+  return true;
 }
 
-export { sendPostToInboxes }
+export { sendPostToInboxes };
