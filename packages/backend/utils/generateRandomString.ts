f707c11dbb8b1f4d52a8f88dfb4805e4547c402b
jbcarreon123 <jbcarreon212@gmail.com>
2025-12-06 17:01:52 +0800

    put to sha1

--- packages/backend/utils/generateRandomString.ts
+++ packages/backend/utils/generateRandomString.ts
@@ -1,7 +1,7 @@
 import crypto from 'crypto'
 
 export default function generateRandomString() {
-  return crypto.createHash('sha2').update(Math.random().toString()).digest('hex')
+  return crypto.createHash('sha1').update(Math.random().toString()).digest('hex')
 }
 
 export function generateRandomStringInviteCode() {
