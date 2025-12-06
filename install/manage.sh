ac0c60666bd355fc229f38e89d7358eea57b1d6b
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-12-06 20:39:49 +0100

    less downtime but worse triming

--- install/manage.sh
+++ install/manage.sh
@@ -99,7 +99,7 @@ case $1 in
       fi
       $SCRIPT_DIR/_auto_updater.sh $OLD_SHA
 
-      docker compose down
+      #docker compose down
       rm -rf packages/backend/cache/ && mkdir packages/backend/cache/ && touch packages/backend/cache/.gitkeep
       docker system prune -f
       docker compose pull
