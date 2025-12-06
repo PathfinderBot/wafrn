6a0bb5941a04f6033915a91aa2fe187e5089d3b4
jbcarreon123 <jbcarreon212@gmail.com>
2025-12-06 16:26:09 +0800

    edit invite code string

--- packages/backend/utils/generateRandomString.ts
+++ packages/backend/utils/generateRandomString.ts
@@ -1,9 +1,9 @@
 import crypto from 'crypto'
 
 export default function generateRandomString() {
-  return crypto.createHash('sha1').update(Math.random().toString()).digest('hex')
+  return crypto.createHash('sha2').update(Math.random().toString()).digest('hex')
 }
 
 export function generateRandomStringInviteCode() {
-  return `${crypto.randomBytes(1).toString('hex')}-${crypto.randomBytes(1).toString('hex')}-${crypto.randomBytes(1).toString('hex')}-${crypto.randomBytes(1).toString('hex')}`
+  return `${crypto.randomBytes(2).toString('hex')}-${crypto.randomBytes(2).toString('hex')}-${crypto.randomBytes(2).toString('hex')}-${crypto.randomBytes(2).toString('hex')}`
 }
