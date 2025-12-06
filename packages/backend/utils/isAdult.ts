4d722f7bf6f7ee25b290f924c5a8b4e749b92dff
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-12-06 23:34:49 +0100

    remove logs

--- packages/backend/utils/isAdult.ts
+++ packages/backend/utils/isAdult.ts
@@ -13,6 +13,5 @@ export function isAdult(date?: string | Date): boolean {
     return false;
   }
   const res = minimumBirthDate.getTime() > birthDate.getTime();
-  logger.debug(`isAdult: ` + res);
   return res;
 }
