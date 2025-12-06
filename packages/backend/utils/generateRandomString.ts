90b369a5ebb9f730fa47af684a07b2bd0dc9698c
jbcarreon123 <jbcarreon212@gmail.com>
2025-12-06 16:30:55 +0800

    not use hex

--- packages/backend/utils/generateRandomString.ts
+++ packages/backend/utils/generateRandomString.ts
@@ -5,5 +5,11 @@ export default function generateRandomString() {
 }
 
 export function generateRandomStringInviteCode() {
-  return `${crypto.randomBytes(2).toString('hex')}-${crypto.randomBytes(2).toString('hex')}-${crypto.randomBytes(2).toString('hex')}-${crypto.randomBytes(2).toString('hex')}`
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const code = Array.from({ length: 4 }, () => {
+    return Array.from({ length: 4 }, () => {
+      return chars[Math.floor(Math.random() * chars.length)];
+    }).join('');
+  }).join('-');
+  return code
 }
