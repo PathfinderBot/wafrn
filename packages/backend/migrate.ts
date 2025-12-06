d40bafbb3746676162997afc661734e96551ba3a
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-12-06 20:33:48 +0100

    Added 2.5 seconds of wait so the database starts

--- packages/backend/migrate.ts
+++ packages/backend/migrate.ts
@@ -1,34 +1,37 @@
-import { Sequelize } from 'sequelize'
-import { Umzug, SequelizeStorage } from 'umzug'
-import process from 'process'
-import { realpathSync } from 'fs'
-import { pathToFileURL } from 'url'
-import { completeEnvironment } from './utils/backendOptions.js'
+import { Sequelize } from "sequelize";
+import { Umzug, SequelizeStorage } from "umzug";
+import process from "process";
+import { realpathSync } from "fs";
+import { pathToFileURL } from "url";
+import { completeEnvironment } from "./utils/backendOptions.js";
+import { wait } from "./utils/wait.js";
 
 function wasCalledAsScript() {
-  const realPath = realpathSync(process.argv[1])
-  const realPathAsUrl = pathToFileURL(realPath).href
-  return import.meta.url === realPathAsUrl
+  const realPath = realpathSync(process.argv[1]);
+  const realPathAsUrl = pathToFileURL(realPath).href;
+  return import.meta.url === realPathAsUrl;
 }
 
-const sequelize = new Sequelize(completeEnvironment.databaseConnectionString)
+await wait(2500);
+
+const sequelize = new Sequelize(completeEnvironment.databaseConnectionString);
 
 const umzug = new Umzug({
-  migrations: { glob: 'migrations/*.ts' },
+  migrations: { glob: "migrations/*.ts" },
   context: sequelize.getQueryInterface(),
   storage: new SequelizeStorage({ sequelize }),
-  logger: console
-})
+  logger: console,
+});
 
 // export the type helper exposed by umzug, which will have the `context` argument typed correctly
-export type Migration = typeof umzug._types.migration
+export type Migration = typeof umzug._types.migration;
 
 if (wasCalledAsScript()) {
-  if (process.argv[2] == 'init-container') {
-    await umzug.up()
-    console.log('Migrations run successfully')
-    process.exit(0)
+  if (process.argv[2] == "init-container") {
+    await umzug.up();
+    console.log("Migrations run successfully");
+    process.exit(0);
   } else {
-    await umzug.runAsCLI()
+    await umzug.runAsCLI();
   }
 }
