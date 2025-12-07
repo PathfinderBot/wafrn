05b67e586afb91dff454eaff9bf44f57d911d73e
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-12-07 23:32:16 +0100

    we just dont send the post to bsky if this is the case

--- packages/backend/utils/queueProcessors/sendPostBsky.ts
+++ packages/backend/utils/queueProcessors/sendPostBsky.ts
@@ -41,105 +41,113 @@ async function sendBiteBsky(biterId: string, postId?: string, userId?: string) {
 
 async function sendPostBsky(job: Job) {
   const post = await Post.findByPk(job.data.postId);
-  if (!post || post.bskyUri) {
-    // post non existing or post already in bsky!
-    return;
-  }
-  const parent = post.parentId ? await Post.findByPk(post.parentId) : undefined;
-  const parentPoster = parent ? await User.findByPk(parent.userId) : undefined;
-  const localUser = await User.findByPk(post.userId);
-  try {
-    if (
-      post.privacy === Privacy.Public &&
-      localUser?.enableBsky &&
-      completeEnvironment.enableBsky
-    ) {
-      if (!parent || parent.bskyUri) {
-        // ok the user has bluesky time to send the post
-        let isReblog = false;
-        if (post.content == "" && post.content_warning == "" && post.parentId) {
-          const mediaCount = await Media.count({
-            where: {
-              postId: post.id,
-            },
-          });
-          const quotesCount = await Quotes.count({
-            where: {
-              quoterPostId: post.id,
-            },
-          });
-          const tagsCount = await PostTag.count({
-            where: {
-              postId: post.id,
-            },
-          });
-          if (mediaCount + quotesCount + tagsCount === 0) {
-            isReblog = true;
-            if (parent?.bskyUri) {
-              let agent = await getAtProtoSession(localUser);
-              const { uri } = await agent.repost(
-                parent.bskyUri,
-                parent.bskyCid as string
-              );
-              post.bskyUri = uri;
-              await post.save();
-            }
-          }
-        }
-        if (!isReblog) {
-          let agent = await getAtProtoSession(localUser);
-          const bskyPost = await agent.post(await postToAtproto(post, agent));
-          await wait(750);
-          const duplicatedPost = await Post.findOne({
-            where: {
-              bskyCid: bskyPost.cid,
-            },
-          });
-          if (duplicatedPost) {
-            logger.debug({
-              message: `Bluesky duplicated post in database already. Cleaning up`,
-            });
-            await Notification.destroy({
+  if (post && !post.bskyUri) {
+    const parent = post.parentId
+      ? await Post.findByPk(post.parentId)
+      : undefined;
+    const parentPoster = parent
+      ? await User.findByPk(parent.userId)
+      : undefined;
+    const localUser = await User.findByPk(post.userId);
+    try {
+      if (
+        post.privacy === Privacy.Public &&
+        localUser?.enableBsky &&
+        completeEnvironment.enableBsky
+      ) {
+        if (!parent || parent.bskyUri) {
+          // ok the user has bluesky time to send the post
+          let isReblog = false;
+          if (
+            post.content == "" &&
+            post.content_warning == "" &&
+            post.parentId
+          ) {
+            const mediaCount = await Media.count({
               where: {
-                postId: duplicatedPost.id,
+                postId: post.id,
               },
             });
-            try {
-              await duplicatedPost.destroy();
-            } catch (err) {
-              duplicatedPost.isDeleted = true;
-              await duplicatedPost.save();
+            const quotesCount = await Quotes.count({
+              where: {
+                quoterPostId: post.id,
+              },
+            });
+            const tagsCount = await PostTag.count({
+              where: {
+                postId: post.id,
+              },
+            });
+            if (mediaCount + quotesCount + tagsCount === 0) {
+              isReblog = true;
+              if (parent?.bskyUri) {
+                let agent = await getAtProtoSession(localUser);
+                const { uri } = await agent.repost(
+                  parent.bskyUri,
+                  parent.bskyCid as string
+                );
+                post.bskyUri = uri;
+                await post.save();
+              }
             }
           }
-          post.bskyUri = bskyPost.uri;
-          post.bskyCid = bskyPost.cid;
-          if (post.parentId) {
-            post.replyControl = 100;
+          if (!isReblog) {
+            let agent = await getAtProtoSession(localUser);
+            const bskyPost = await agent.post(await postToAtproto(post, agent));
+            await wait(750);
+            const duplicatedPost = await Post.findOne({
+              where: {
+                bskyCid: bskyPost.cid,
+              },
+            });
+            if (duplicatedPost) {
+              logger.debug({
+                message: `Bluesky duplicated post in database already. Cleaning up`,
+              });
+              await Notification.destroy({
+                where: {
+                  postId: duplicatedPost.id,
+                },
+              });
+              try {
+                await duplicatedPost.destroy();
+              } catch (err) {
+                duplicatedPost.isDeleted = true;
+                await duplicatedPost.save();
+              }
+            }
+            post.bskyUri = bskyPost.uri;
+            post.bskyCid = bskyPost.cid;
+            if (post.parentId) {
+              post.replyControl = 100;
+            }
+            await post.save();
           }
-          await post.save();
         }
       }
+    } catch (error) {
+      logger.debug({ message: `Error sending post to bluesky`, error });
     }
-  } catch (error) {
-    logger.debug({ message: `Error sending post to bluesky`, error });
   }
-  const prepareSendPostQueue = new Queue("prepareSendPost", {
-    connection: completeEnvironment.bullmqConnection,
-    defaultJobOptions: {
-      removeOnComplete: true,
-      attempts: 3,
-      backoff: {
-        type: "exponential",
-        delay: 1000,
+  if (post) {
+    const prepareSendPostQueue = new Queue("prepareSendPost", {
+      connection: completeEnvironment.bullmqConnection,
+      defaultJobOptions: {
+        removeOnComplete: true,
+        attempts: 3,
+        backoff: {
+          type: "exponential",
+          delay: 1000,
+        },
+        removeOnFail: true,
       },
-      removeOnFail: true,
-    },
-  });
-  // we send the post to fedi once we get the bsky data
-  await prepareSendPostQueue.add("prepareSendPost", job.data, {
-    jobId: post.id,
-    delay: 1500,
-  });
+    });
+    // we send the post to fedi once we get the bsky data
+    await prepareSendPostQueue.add("prepareSendPost", job.data, {
+      jobId: post.id,
+      delay: 1500,
+    });
+  }
 }
 
 export { sendPostBsky, sendBiteBsky };
