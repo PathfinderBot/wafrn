b85eba81447149d5778d39b4b3bbd8c87901f2ed
alexia <cyrus42@disroot.org>
2025-12-15 15:40:31 +0100

    did an oopsie

--- shell.nix
+++ shell.nix
@@ -7,6 +7,6 @@ pkgs.mkShell {
   shellHook = ''
   echo "running npm install..."
   npm install . 
-  echo -e "\r\nUse `npm run frontend:develop:prod` to start the development server, make sure to clear your cache if you run a switch servers!"
+  echo -e "\r\nUse \`npm run frontend:develop:prod\` to start the development server, make sure to clear your cache if you run a switch servers!"
   '';
 }
