08c78d2c5e9fce6b17452f3ffab97db2f167d01c
alexia <cyrus42@disroot.org>
2025-12-15 15:26:19 +0100

    improve shell.nix
    auto-run npm install, clean up the nix code a bit

--- shell.nix
+++ shell.nix
@@ -4,5 +4,9 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [ nodejs_24 jq ];
-  shellHook = "echo \"Use \"npm run frontend:develop:prod\" to start the development server, make sure to clear your cache if you run a switch servers!\"";
+  shellHook = ''
+  echo "running npm install..."
+  npm install . 
+  echo -e "\r\nUse `npm run frontend:develop:prod` to start the development server, make sure to clear your cache if you run a switch servers!"
+  '';
 }
