8d3b6803c2ab48781469357bc578ac54c182a589
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2025-12-17 21:31:27 +0100

    more stats

--- packages/frontend/src/app/pages/admin/stats/stats.component.ts
+++ packages/frontend/src/app/pages/admin/stats/stats.component.ts
@@ -1,26 +1,27 @@
-import { Component, inject } from '@angular/core'
-import { MatCardModule } from '@angular/material/card'
-import { TranslateModule } from '@ngx-translate/core'
-import { statsReply } from 'src/app/interfaces/statsReply'
-import { AdminService } from 'src/app/services/admin.service'
-import { SimpleTitleService } from 'src/app/services/simple-title.service'
+import { JsonPipe } from "@angular/common";
+import { Component, inject } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { TranslateModule } from "@ngx-translate/core";
+import { statsReply } from "src/app/interfaces/statsReply";
+import { AdminService } from "src/app/services/admin.service";
+import { SimpleTitleService } from "src/app/services/simple-title.service";
 
 @Component({
-  selector: 'app-stats',
-  imports: [MatCardModule, TranslateModule],
-  templateUrl: './stats.component.html',
-  styleUrl: './stats.component.scss'
+  selector: "app-stats",
+  imports: [MatCardModule, TranslateModule, JsonPipe],
+  templateUrl: "./stats.component.html",
+  styleUrl: "./stats.component.scss",
 })
 export class StatsComponent {
-  backendReply: statsReply | undefined
+  backendReply: statsReply | undefined;
   constructor() {
     const adminService = inject(AdminService);
     const simpleTitle = inject(SimpleTitleService);
 
-    simpleTitle.set('menu.admin.stats')
+    simpleTitle.set("menu.admin.stats");
 
     adminService.getStats().then((response) => {
-      this.backendReply = response
-    })
+      this.backendReply = response;
+    });
   }
 }
