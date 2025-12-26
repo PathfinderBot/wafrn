a7d3cf75064d51f4c1ba3bfc1b38f9ba85c83c19
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2025-12-26 21:22:59 +0100

    no cache

--- packages/backend/utils/cacheGetters/getUserOptions.ts
+++ packages/backend/utils/cacheGetters/getUserOptions.ts
@@ -1,19 +1,13 @@
 import { UserOptions } from '../../models/index.js'
 import { redisCache } from '../redis.js'
 
-async function getUserOptions(userId: string, forceTryCache?: boolean): Promise<Array<{ optionName: string; optionValue: string }>> {
-  const cacheReply = forceTryCache ? (await redisCache.get('userOptions:' + userId)) : undefined
-  if (cacheReply) {
-    return JSON.parse(cacheReply)
-  } else {
+async function getUserOptions(userId: string): Promise<Array<{ optionName: string; optionValue: string }>> {
     const dbReply = await UserOptions.findAll({
       where: {
         userId: userId
       }
     })
-    redisCache.set('userOptions:' + userId, JSON.stringify(dbReply.map((elem: any) => elem.dataValues)), 'EX', 600)
-    return getUserOptions(userId, true)
-  }
+    return dbReply.map((elem: any) => elem.dataValues)
 }
 
 export { getUserOptions }
