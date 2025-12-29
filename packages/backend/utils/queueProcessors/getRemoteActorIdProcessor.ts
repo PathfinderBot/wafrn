1d1d1890cf615e8ebf5d58404eecc846450f3790
jbcarreon123 <jbcarreon212@gmail.com>
2025-12-29 11:02:38 +0800

    heh

--- packages/backend/utils/queueProcessors/getRemoteActorIdProcessor.ts
+++ packages/backend/utils/queueProcessors/getRemoteActorIdProcessor.ts
@@ -351,7 +351,7 @@ async function getRemoteActorIdProcessor(job: Job) {
           }
 
           try {
-            const atUri = (userPetition.alsoKnownAs as string[]).find(x => x.startsWith('did:'))
+            const atUri = (userPetition.alsoKnownAs as string[]).find(x => x.startsWith('did:') || x.startsWith('at://'))
             let mergeAcc = 0
             if (atUri) {
               const atDoc = await getDidDoc(atUri)
