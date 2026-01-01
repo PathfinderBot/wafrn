72f2fa06fb1c0b88f0771acc6043549fe9bcae49
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-01-01 20:32:10 +0000

    dupe index

--- packages/backend/websocket.ts
+++ packages/backend/websocket.ts
@@ -104,13 +104,13 @@ if (
   logger.info(
     `ATTENTION: your server doesnt seem to have an unique index on bskyuri. this is a bug. we will investigate soon in a future release`
   );
-  // clearDuplicatedBskyUris().then(async (res) => {
+  clearDuplicatedBskyUris().then(async (res) => {
   //   // well turns out that we dont have indexes
   //   // we have cleaned duplicated before. if a duplicate apears here we just crash and do it again :3
-  //   await queryInterface.sequelize.query(
-  //     `CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS post_bsky_uri  ON "posts" ("bskyUri");`
-  //   );
-  // });
+  await queryInterface.sequelize.query(
+    `CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS post_bsky_uri  ON "posts" ("bskyUri");`
+  );
+   });
 }
 
 async function clearDuplicatedBskyUris(): Promise<boolean> {
