3e07ca959630b402aa5f53bd4577bb038eb55717
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-01-07 23:25:08 +0100

    Force update password of user on bsky

---
+++ packages/backend/utils/maintenanceTasks/forceUpdateBskyPassword.ts
@@ -0,0 +1,31 @@
+import { Op } from "sequelize";
+import { User } from "../../models/user.js";
+import { completeEnvironment } from "../backendOptions.js";
+import { forceUpdateBskyPassword } from "../atproto/updateUserDidDoc.js";
+
+
+
+const args = process.argv.slice(2);
+
+console.log(args)
+
+const username = args[0]
+
+const user = await User.findOne({
+    where: {
+        url: {
+            [Op.iLike]: username
+        }
+    }
+})
+
+if(user && user.enableBsky && completeEnvironment.enableBsky) {
+    let agent = await forceUpdateBskyPassword(user)
+    if(agent) {
+        console.log('succ ess')
+    } else {
+        console.log('error')
+    }
+} else {
+    console.log('Failed to find '+ username + ' or they dont have bsky enabled')
+}
