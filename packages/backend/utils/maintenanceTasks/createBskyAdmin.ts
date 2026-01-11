b0ab3ee0fc88439fb79d1ade71c6f5ff158eabb9
Juan D. Jara <juanorigami@gmail.com>
2026-01-11 02:53:39 +0100

    format and remove unused

--- packages/backend/utils/maintenanceTasks/createBskyAdmin.ts
+++ packages/backend/utils/maintenanceTasks/createBskyAdmin.ts
@@ -1,52 +1,45 @@
-import { Op } from "sequelize";
-import { User } from "../../models/user.js";
-import { completeEnvironment } from "../backendOptions.js";
-import { forceUpdateBskyPassword } from "../atproto/updateUserDidDoc.js";
-import { getAdminUser } from "../getAdminAndDeletedUser.js";
-import { BskyInviteCodes } from "../../models/index.js";
-import { AtpAgent } from "@atproto/api";
-import { createBskyAccount } from "../../routes/users.js";
-import generateRandomString from "../generateRandomString.js";
+import { completeEnvironment } from '../backendOptions.js'
+import { getAdminUser } from '../getAdminAndDeletedUser.js'
+import { BskyInviteCodes } from '../../models/index.js'
+import { AtpAgent } from '@atproto/api'
+import { createBskyAccount } from '../../routes/users.js'
+import generateRandomString from '../generateRandomString.js'
 
-
-
-const args = process.argv.slice(2);
-
+const args = process.argv.slice(2)
 
 const name = args[0]
 
 const user = await getAdminUser()
 
-if(user && (!user.enableBsky || !user.bskyDid) && completeEnvironment.enableBsky) {
-    console.log(`Trying to create user: @${name + '.' + completeEnvironment.bskyPds}`)
-    const inviteCodeRecord = await BskyInviteCodes.findOne({
-              where: {
-                masterCode: true,
-              },
-            });
-    const inviteCode = inviteCodeRecord?.code as string;
-    const serviceUrl = completeEnvironment.bskyPds
-        ? completeEnvironment.bskyPds.startsWith("http")
-            ? completeEnvironment.bskyPds
-            : "https://" + completeEnvironment.bskyPds
-        : "";
-    const agent = new AtpAgent({
-                service: serviceUrl,
-              });
-    const password = generateRandomString()
-    await createBskyAccount({
-            agent,
-            user,
-            password,
-            inviteCode,
-            url: name
-          });
-    console.log('Done! Please do recheck now')
-
+if (user && (!user.enableBsky || !user.bskyDid) && completeEnvironment.enableBsky) {
+  console.log(`Trying to create user: @${name + '.' + completeEnvironment.bskyPds}`)
+  const inviteCodeRecord = await BskyInviteCodes.findOne({
+    where: {
+      masterCode: true
+    }
+  })
+  const inviteCode = inviteCodeRecord?.code as string
+  const serviceUrl = completeEnvironment.bskyPds
+    ? completeEnvironment.bskyPds.startsWith('http')
+      ? completeEnvironment.bskyPds
+      : 'https://' + completeEnvironment.bskyPds
+    : ''
+  const agent = new AtpAgent({
+    service: serviceUrl
+  })
+  const password = generateRandomString()
+  await createBskyAccount({
+    agent,
+    user,
+    password,
+    inviteCode,
+    url: name
+  })
+  console.log('Done! Please do recheck now')
 } else {
-    console.log(`It seems that ${user.url} has enabled bsky already or your server has bsky disabled`)
-    console.log(`Server bsky enabled: ${completeEnvironment.enableBsky}`)
-    console.log(`admin account did: ${user.bskyDid}`)
-    console.log(`admin account bskyenabled: ${user.enableBsky}`)
-    console.log(`Hope this can help you debug. Remember to check adminer!`)
+  console.log(`It seems that ${user.url} has enabled bsky already or your server has bsky disabled`)
+  console.log(`Server bsky enabled: ${completeEnvironment.enableBsky}`)
+  console.log(`admin account did: ${user.bskyDid}`)
+  console.log(`admin account bskyenabled: ${user.enableBsky}`)
+  console.log(`Hope this can help you debug. Remember to check adminer!`)
 }
