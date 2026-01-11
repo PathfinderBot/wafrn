b384643e4f97c0de73a20fb5caf2246eebd8e03c
Juan D. Jara <juanorigami@gmail.com>
2026-01-11 03:00:11 +0100

    extract sendBiteBsky to its own file

--- packages/backend/utils/queueProcessors/sendPostBsky.ts
+++ packages/backend/utils/queueProcessors/sendPostBsky.ts
@@ -7,31 +7,6 @@ import { postToAtproto } from '../../atproto/utils/postToAtproto.js'
 import { wait } from '../wait.js'
 import { logger } from '../logger.js'
 
-async function sendBiteBsky(biterId: string, postId?: string, userId?: string) {
-  const post = postId ? await Post.findByPk(postId) : undefined
-  const user = userId ? await User.findByPk(userId) : undefined
-  const localUser = await User.findByPk(biterId)
-
-  if ((post && !post?.bskyUri) || (user && !user.bskyDid)) return // make sure there is actually a bsky user
-  if (!localUser || !localUser.enableBsky || !localUser.bskyDid) return // make sure there's actually a bsky account to put the record on
-
-  const subjectUri = post ? post.bskyUri : user ? 'at://' + user.bskyDid : ''
-
-  const bskyBite = {
-    $type: 'net.wafrn.feed.bite',
-    subject: subjectUri,
-    createdAt: new Date(Date.now()).toISOString()
-  }
-
-  let agent = await getAtProtoSession(localUser)
-
-  await agent.com.atproto.repo.createRecord({
-    repo: agent.session?.did ?? localUser.bskyDid ?? '',
-    collection: 'net.wafrn.feed.bite',
-    record: bskyBite
-  })
-}
-
 async function sendPostBsky(job: Job) {
   const post = await Post.findByPk(job.data.postId)
   if (post && !post.bskyUri) {
@@ -134,4 +109,4 @@ async function sendPostBsky(job: Job) {
   }
 }
 
-export { sendPostBsky, sendBiteBsky }
+export { sendPostBsky }
