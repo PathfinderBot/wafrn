05449213bc2f72c1362f174f157d8730485f9db5
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-01-18 19:00:49 +0100

    Fix privacy of posts of user whose posts were in the wrong privacy

---
+++ packages/backend/migrations/2026.01.18T17.54.25.fix-privacy-user.ts
@@ -0,0 +1,19 @@
+import { DataTypes, Sequelize } from 'sequelize'
+import { Migration } from '../migrate.js'
+import { EmojiCollection } from '../models/emojiCollection.js'
+import { Emoji } from '../models/emoji.js'
+import { wait } from '../utils/wait.js'
+
+export const up: Migration = async (params) => {
+  const queryInterface = params.context
+  const userIdtoFix = await queryInterface.sequelize.query(`SELECT "id" FROM "users" WHERE "url" ILIKE '@silt@cofe.rocks'`)
+  if(userIdtoFix[0]) {
+    let userId = (userIdtoFix[0][0] as {id: string} | undefined)?.id
+    if(userId) {
+        await queryInterface.sequelize.query(`UPDATE "posts" SET "privacy"=1 WHERE "userId"='${userId}' AND "privacy"=2`)
+    }
+  }
+}
+export const down: Migration = async (params) => {
+  // nah
+}
