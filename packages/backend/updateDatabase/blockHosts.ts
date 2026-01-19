f63e57782c3a82bfb19131748176b18ab86d575a
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-01-19 23:35:06 +0100

    block worst of the worst

--- packages/backend/updateDatabase/blockHosts.ts
+++ packages/backend/updateDatabase/blockHosts.ts
@@ -6,8 +6,13 @@ import { completeEnvironment } from '../utils/backendOptions.js'
 async function blockHosts() {
   // At some point this will get cooler and will offer the user the option to use THEBADSPACE. It doesnt matter if I do have opinions. Users need choices.
   const remoteData = await axios.get('https://seirdy.one/pb/FediNuke.txt') 
+  const iftasData = await axios.get('https://about.iftas.org/wp-content/uploads/2025/10/iftas-dni-latest.csv')
   console.log('remote data obtained')
-  const hostLines: string[] = remoteData.data.split('\n')
+  let hostLines: string[] = remoteData.data.split('\n')
+  const iftasHosts: string[] = iftasData.data.split('\n')
+  iftasHosts.shift();
+  hostLines = hostLines.concat(iftasHosts.map(elem => elem.split(',')[0]))
+  
   //hostLines.forEach(async (line, index) => {
   let index = 0
   console.log('initiating marking of problematic hosts')
