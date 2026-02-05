e63e749dce87277f809bdd18618b0df9c1b380d8
Jb Carreon <jb@jbc.lol>
2026-02-05 14:55:16 +0800

    remove the subcomponent str req

--- packages/backend/utils/getUserAgent.ts
+++ packages/backend/utils/getUserAgent.ts
@@ -3,7 +3,6 @@ import { completeEnvironment } from '../utils/backendOptions.js'
 
 const packageJsonFile = JSON.parse(fs.readFileSync('package.json').toString())
 
-// subcomponent should be ComponentName/Version
 export default function getUserAgent(subcomponent: string = "") {
   return `Wafrn/${packageJsonFile.version} (${completeEnvironment.frontendUrl}; +${completeEnvironment.adminEmail})${subcomponent? ' ' + subcomponent + '/' + packageJsonFile.version : ''}`
 }
