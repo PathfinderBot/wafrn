646ef5a206ee9bc15b0e02f33947137d8f11c6f9
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-03-02 17:03:46 +0100

    Fixed bug with content negotiation

--- packages/backend/utils/activitypub/checkContentNegotiation.ts
+++ packages/backend/utils/activitypub/checkContentNegotiation.ts
@@ -14,7 +14,7 @@ function getCheckContentNegotiation(allowedContentTypes: string[] = defaultAllow
     next: NextFunction,
   ) {
     if (req.fediData
-        && req.fediData?.valid
+        && !req.fediData?.valid
         && req.headers.accept
         && !allowedContentTypes.some(type => req.headers.accept?.includes(type))) {
       // Respond with a 401 if the request did not include an Accept header for a HTML response.
