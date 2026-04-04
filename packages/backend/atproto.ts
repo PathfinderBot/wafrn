6babec5cc0b21785c4ca6dbb3f1026c780b46855
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-04-04 23:50:24 +0200

    retry only once

--- packages/backend/atproto.ts
+++ packages/backend/atproto.ts
@@ -43,12 +43,8 @@ const firehoseQueue = new Queue("firehoseQueue", {
   connection: completeEnvironment.bullmqConnection,
   defaultJobOptions: {
     removeOnComplete: true,
-    attempts: 3,
-    backoff: {
-      type: "exponential",
-      delay: 25000,
-    },
-    removeOnFail: false,
+    attempts: 2,
+    removeOnFail: true,
   },
 });
 
