8467c9ecaa405b677dafc383f97aa2c914933594
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-04-20 23:39:01 +0200

    whops

--- packages/backend/migrations/2026.04.19T11.48.23.loadRedisKeys.ts
+++ packages/backend/migrations/2026.04.19T11.48.23.loadRedisKeys.ts
@@ -12,16 +12,16 @@ export const up: Migration = async (params) => {
     const queryInterface = params.context;
     // lets get all dids that are followed and all the local dids
     const localUserDidsQuery = (await queryInterface.sequelize.query(`SELECT "bskyDid" FROM "users" WHERE "email" IS NOT NULL and "bskyDid" IS NOT NULL`))[0].map((elem: any) => elem.bskyDid) as string[]
-    if(localUserDidsQuery) {
+    if(localUserDidsQuery.length) {
           await redisCache.sadd(LOCAL_USER_DIDS_CACHE_KEY,localUserDidsQuery)
     }
     const followedUsersDids: string[] = (await queryInterface.sequelize.query(`SELECT "bskyDid" FROM "users" WHERE "bskyDid" IS NOT NULL AND "id" IN (SELECT "followedId" FROM "follows" WHERE "followerId" IN (SELECT "id" FROM "users" WHERE "email" IS NOT NULL))`))[0].map((elem: any) => elem.bskyDid )
-    if(followedUsersDids) {
+    if(followedUsersDids.length) {
       await redisCache.sadd(FOLLOWED_BSKY_DIDS_CACHE_KEY, followedUsersDids)
 
     }
     const followedHashtags: string[] = (await queryInterface.sequelize.query(`SELECT DISTINCT "tagName" FROM "userFollowHashtags"`))[0].map((elem: any) => elem.tagName.toLowerCase())
-    if(followedHashtags) {
+    if(followedHashtags.length) {
       await redisCache.sadd(FOLLOWED_HASHTAGS_CACHE_KEY, followedHashtags)
     }
     const repliedPosts: string[] = (await queryInterface.sequelize.query(`SELECT "bskyUri" FROM "posts" WHERE "id" IN (SELECT "ancestorId" FROM "postsancestors" WHERE "postsId" IN (SELECT "id" FROM  "posts" where "userId" IN (select "id" FROM "users" WHERE "email" IS NOT NULL) AND "hierarchyLevel"!=1 AND "bskyUri" IS NOT NULL ORDER BY "createdAt" DESC LIMIT 10000)) and "hierarchyLevel" = 1 and "bskyUri" is NOT NULL`))[0].map((elem: any) => elem.bskyUri )
