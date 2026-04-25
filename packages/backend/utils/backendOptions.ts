200109e8b11cd40f8588029debc8562b0a47b741
Gabriel Amador <gabriel.amador.garcia@gmail.com>
2026-04-25 16:12:27 +0200

    load environment from server

--- packages/backend/utils/backendOptions.ts
+++ packages/backend/utils/backendOptions.ts
@@ -14,10 +14,6 @@ export const completeEnvironment = {
     instanceName: baseEnvironment.instanceUrl,
     enableBsky: baseEnvironment.enableBsky,
     bskyPDSUrl: baseEnvironment.bskyPdsUrl ? baseEnvironment.bskyPdsUrl : baseEnvironment.bskyPds,
-    baseUrl:
-      baseEnvironment.frontendEnvironment.baseUrl === '/api'
-        ? `${baseEnvironment.frontendUrl}/api`
-        : baseEnvironment.frontendEnvironment.baseUrl
     // the 'satisfies' keyword is used to tell typescript that this object is fits with type Environment but can extend it
     // for example, to make the 'bskyPdsUrl' property not optional
   } satisfies Environment
