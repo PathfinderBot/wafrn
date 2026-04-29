a1f6772d9e1ed5c91b061c685c78c665eb1e8abd
umaruru <deolindojvn@gmail.com>
2026-04-29 13:03:32 -0300

    remove postLanguage table (cleanup)

--- packages/backend/migrations/2026.04.26T15.58.12.addPostLanguage.ts
+++ packages/backend/migrations/2026.04.26T15.58.12.addPostLanguage.ts
@@ -9,39 +9,9 @@ export const up: Migration = async (params) => {
 		defaultValue: null,
 		allowNull: true,
 	});
-
-	// await queryInterface.createTable('postLanguage', {
-	// 	"postId": {
-	// 		type: DataTypes.UUID,
-	// 		onUpdate: "CASCADE",
-	// 		onDelete: "CASCADE",
-	// 		references: {
-	// 			model: "posts",
-	// 			key: "id",
-	// 		},
-	// 		unique: "postLanguage_postId_unique",
-	// 		field: "postId",
-	// 		primaryKey: true,
-	// 		allowNull: false,
-	// 	},
-	// 	"language": {
-	// 		type: DataTypes.STRING(3),
-	// 		field: "language",
-	// 		allowNull: false,
-	// 	}
-	// });
-
-	// await queryInterface.addIndex('postLanguage',
-	// 	["language"],
-	// 	{
-	// 		name: "postLanguage_language",
-	// 		unique: false,
-	// 	}
-	// )
 }
 
 export const down: Migration = async (params) => {
 	const queryInterface = params.context;
 	await queryInterface.removeColumn('posts', 'language');
-	// await queryInterface.dropTable('postLanguage');
 }
