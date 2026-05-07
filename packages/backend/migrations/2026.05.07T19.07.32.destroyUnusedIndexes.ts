ad0c4e55f9b4365afc4c8744c84228c983911656
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2026-05-07 21:43:25 +0200

    removed unused indexes

---
+++ packages/backend/migrations/2026.05.07T19.07.32.destroyUnusedIndexes.ts
@@ -0,0 +1,22 @@
+import { DataTypes, Sequelize, UUIDV4 } from "sequelize";
+import { Migration } from "../migrate.js";
+import { FederatedHost } from "../models/federatedHost.js";
+import { User } from "../models/user.js";
+import { DataType } from "sequelize-typescript";
+
+export const up: Migration = async (params) => {
+  const queryInterface = params.context;
+  await queryInterface.sequelize.query(
+    `DROP INDEX IF EXISTS "posts_created_at_privacy";
+    DROP INDEX IF EXISTS "posts_created_at_desc";
+    DROP INDEX IF EXISTS "mediaUrlIndex";
+    `
+
+  );
+  
+
+};
+export const down: Migration = async (params) => {
+
+
+};
