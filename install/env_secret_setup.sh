fe8de54a3abdad50c2401a5f450296ed58010146
Juan D. Jara <juanorigami@gmail.com>
2026-05-15 13:39:53 +0200

    comment env_secret_setup.sh

--- install/env_secret_setup.sh
+++ install/env_secret_setup.sh
@@ -4,28 +4,43 @@
 #
 # $ ./install/env_secret_setup.sh
 
+echo creating JWT_SECRET
 export JWT_SECRET="$(openssl rand -base64 64 | tr -d '\n')"
+
+echo creating POSTGRES_PASSWORD
 export POSTGRES_PASSWORD="$(openssl rand -base64 24 | tr '+/' '_-')"
+
+echo creating PDS_JWT_SECRET
 export PDS_JWT_SECRET="$(openssl rand --hex 16)"
+
+echo creating PDS_ADMIN_PASSWORD
 export PDS_ADMIN_PASSWORD="$(openssl rand -base64 24 | tr '+/' '_-')"
+
+echo creating POSTGRES_METRICS_PASSWORD
 export POSTGRES_METRICS_PASSWORD="$(openssl rand -base64 24 | tr '+/' '_-')"
+
+echo creating PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX
 export PDS_PLC_ROTATION_KEY_K256_PRIVATE_KEY_HEX="$(openssl ecparam --name secp256k1 --genkey --noout --outform DER | tail --bytes=+8 | head --bytes=32 | xxd --plain --cols 32)"
 
+echo creating webpush keys
 VAPID_KEYS="$(docker run -e NPM_CONFIG_UPDATE_NOTIFIER=false packageless/npx:latest --yes web-push generate-vapid-keys --json)"
-
 export WEBPUSH_PRIVATE="$(echo "$VAPID_KEYS" | jq -r .privateKey)"
 export WEBPUSH_PUBLIC="$(echo "$VAPID_KEYS" | jq -r .publicKey)"
 export WEBPUSH_EMAIL="mailto:$ADMIN_EMAIL"
 
 # this might be set earlier
 if [ -z "${ADMIN_PASSWORD}" ]; then
+  echo creating ADMIN_PASSWORD
   export ADMIN_PASSWORD="$(openssl rand -base64 24 | tr '+/' '_-')"
 fi
 
 if [ -z "${GF_SECURITY_ADMIN_PASSWORD}" ]; then
+  echo creating GF_SECURITY_ADMIN_PASSWORD
   export GF_SECURITY_ADMIN_PASSWORD="$(openssl rand -base64 24 | tr '+/' '_-')"
 fi
 
 cp .env.example .env
 
 perl -pi -e 's/^([_A-Z0-9]+)=(.*)$/$1."='"'"'".($ENV{$1}||$2)."'"'"'"/ge' .env
+
+echo "Done! All env variables saved to .env"
