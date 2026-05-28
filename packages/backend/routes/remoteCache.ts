75a1526bf006da670059058f1427d5896232de6b
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2026-05-28 22:22:51 +0200

    whops

--- packages/backend/routes/remoteCache.ts
+++ packages/backend/routes/remoteCache.ts
@@ -302,7 +302,7 @@ async function getMediaFromUrl(mediaUrl: string, res?: Response, force = false, 
     }
 
     // Try to acquire lock in Redis
-    const lockAcquired = await redisCache.get(lockKey) ? await redisCache.set(lockKey, lockValue, 'EX', 30) : false
+    const lockAcquired = await redisCache.get(lockKey) ? false : await redisCache.set(lockKey, lockValue, 'EX', 30)
 
     if (lockAcquired) {
       // We have the lock, proceed with download
