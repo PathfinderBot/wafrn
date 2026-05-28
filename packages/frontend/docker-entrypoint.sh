b0995829ecf9f7f77ddbe2e954984d400b79e55d
Juan D. Jara <juanorigami@gmail.com>
2026-05-28 15:02:47 +0200

    temp disable

--- packages/frontend/docker-entrypoint.sh
+++ packages/frontend/docker-entrypoint.sh
@@ -11,6 +11,7 @@ perl -pi -e 's/\$\{\{([_A-Z]+):-(.*)\}\}/$ENV{$1}||$2/ge' /var/www/html/frontend
 perl -pi -e 's/\$\{\{([_A-Z]+)\}\}/$ENV{$1}/g' /var/www/html/frontend/manifest.webmanifest
 
 # this command performs the runtime overrides for people using the pre-compiled images
-cp -af /overrides/* /var/www/html/frontend || true
+# cp -af /overrides/* /var/www/html/frontend || true
 
 exec "$@"
+
