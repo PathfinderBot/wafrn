2ef0d6be83de6f65ca35863013038a61345f86bb
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2026-06-01 08:02:16 +0200

    I fucked up I think

--- packages/frontend/docker-entrypoint.sh
+++ packages/frontend/docker-entrypoint.sh
@@ -10,11 +10,6 @@ perl -pi -e 's/\$\{\{([_A-Z]+)\}\}/$ENV{$1}/g' /var/www/html/frontend/index.html
 perl -pi -e 's/\$\{\{([_A-Z]+):-(.*)\}\}/$ENV{$1}||$2/ge' /var/www/html/frontend/manifest.webmanifest
 perl -pi -e 's/\$\{\{([_A-Z]+)\}\}/$ENV{$1}/g' /var/www/html/frontend/manifest.webmanifest
 
-cp -r /var/www/html/frontend_output /var/www/html/frontend/
-#if [ -d /overrides ] && [ "$(find /overrides -mindepth 1 -maxdepth 1 | head -n 1)" ]; then
-#  /overrides.sh /overrides /var/www/html/frontend/ngsw.json
-#else
-#  echo "WARNING: OVERRIDES FOLDER MISSING"
-#fi
+/overrides.sh /overrides /var/www/html/frontend/ngsw.json
 
 exec "$@"
