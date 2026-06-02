5fcd4185c4e1e4da8db827fead94c6c587e69267
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2026-06-02 17:40:37 +0200

    lets test this

--- packages/frontend/docker-entrypoint.sh
+++ packages/frontend/docker-entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 # Copy built frontend files to the volume
 # This ensures we always serve the latest built version, even if the volume persists from a previous container run
-cp -a /usr/share/wafrn/frontend/* /var/www/html/frontend/
+mkdir -p /var/www/html/frontend
+find /var/www/html/frontend -mindepth 1 -maxdepth 1 -exec rm -rf {} +
+cp -a /usr/share/wafrn/frontend/. /var/www/html/frontend/
 
 # replace all environment variables in the files in the frontend volume before starting caddy
 perl -pi -e 's/NGSW_([_A-Z]+)/$ENV{$1}/g' /var/www/html/frontend/ngsw.json
