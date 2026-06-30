f9abd040f8f97f17424fa081edbc0163817a29eb
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2026-06-30 10:28:35 +0200

    indexes, grafana

---
+++ packages/backend/migrations/2026.06.30T07.05.16.rootIdIsDeleted.ts
@@ -0,0 +1,17 @@
+import { Migration } from "../migrate.js";
+import { sequelize } from "../models/index.js";
+
+export const up: Migration = async (params) => {
+    const queryInterface = params.context;
+    const updateQuery = await sequelize.query(`
+        CREATE INDEX IF EXISTS idx_posts_rootid_active
+ON posts ("rootId")
+WHERE "isDeleted" = false
+        `)
+
+};
+export const down: Migration = async (params) => {
+    const queryInterface = params.context;
+    await sequelize.query(`DROP INDEX IF EXISTS idx_posts_rootid_active`)
+
+};
