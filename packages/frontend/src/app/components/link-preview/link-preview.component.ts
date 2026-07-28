3a32eeb088c4ffb521112fbf4872d13a6e4ea25d
Gabriel Amador García <gabriel.amador.garcia@gmail.com>
2026-07-28 21:09:31 +0200

    fixes

--- packages/frontend/src/app/components/link-preview/link-preview.component.ts
+++ packages/frontend/src/app/components/link-preview/link-preview.component.ts
@@ -35,47 +35,49 @@ export class LinkPreviewComponent implements OnChanges {
   forceYoutube = false
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.forceTenorGif = false
-    this.forceYoutube = false
-    if (this.link) {
-      if (this.url.startsWith('https://media.tenor.com/') || this.url.startsWith('https://static.klipy.com')) {
-        this.loading = false
-        this.forceTenorGif = true
-        return
-      }
-      this.loading = true
-      const linkToGet = this.link.startsWith(EnvironmentService.environment.externalCacheurl)
-      try {
-        this.url = linkToGet
-          ? (new URL(this.link, EnvironmentService.environment.frontUrl).searchParams.get('media') as string)
-          : this.link
-      } catch (error) {
-        this.url = this.link
-        console.log(this.link)
-      }
+    try {
+      this.forceTenorGif = false
+      this.forceYoutube = false
+      if (this.link) {
+        if (this.url.startsWith('https://media.tenor.com/') || this.url.startsWith('https://static.klipy.com')) {
+          this.loading = false
+          this.forceTenorGif = true
+          return
+        }
+        this.loading = true
+        const linkToGet = this.link.startsWith(EnvironmentService.environment.externalCacheurl)
+        try {
+          this.url = linkToGet
+            ? (new URL(this.link, EnvironmentService.environment.frontUrl).searchParams.get('media') as string)
+            : this.link
+        } catch (error) {
+          this.url = this.link
+          console.log(this.link)
+        }
 
-      this.hostname = new URL(this.url).hostname
-      this.mediaService.getLinkPreview(this.url).then((data) => {
-        this.favicon = EnvironmentService.environment.externalCacheurl + encodeURIComponent(data.favicons.at(0))
-        this.loading = false
-        if (data.images && data.images.length) {
-          this.img = EnvironmentService.environment.externalCacheurl + encodeURIComponent(data.images[0])
-        }
-        if (!this.img && data.favicons && data.favicons.length) {
-          this.img = this.favicon
-        }
-        let sitenamePrefix = ''
-        if (data.siteName) {
-          sitenamePrefix = data.siteName + ' - '
-        }
-        if (data.title) {
-          this.title = sitenamePrefix + data.title
-        }
-        if (data.description) {
-          this.description = data.description
-        }
-        this.cdr.detectChanges()
-      })
-    }
+        this.hostname = new URL(this.url).hostname
+        this.mediaService.getLinkPreview(this.url).then((data) => {
+          this.favicon = EnvironmentService.environment.externalCacheurl + encodeURIComponent(data.favicons.at(0))
+          this.loading = false
+          if (data.images && data.images.length) {
+            this.img = EnvironmentService.environment.externalCacheurl + encodeURIComponent(data.images[0])
+          }
+          if (!this.img && data.favicons && data.favicons.length) {
+            this.img = this.favicon
+          }
+          let sitenamePrefix = ''
+          if (data.siteName) {
+            sitenamePrefix = data.siteName + ' - '
+          }
+          if (data.title) {
+            this.title = sitenamePrefix + data.title
+          }
+          if (data.description) {
+            this.description = data.description
+          }
+          this.cdr.detectChanges()
+        })
+      }
+    } catch (error) {}
   }
 }
