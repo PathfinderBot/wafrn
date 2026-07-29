65f18981c6c26363bb0b955520b9c31346e43370
EveryDayCompute <everydaycompute@noreply.codeberg.org>
2026-07-29 08:48:32 +0200

    Fix not working in certain firefox versions such as dev
    The preventDefault call is needed for the drag to function

--- packages/frontend/src/app/components/new-editor/new-editor.component.ts
+++ packages/frontend/src/app/components/new-editor/new-editor.component.ts
@@ -948,6 +948,7 @@ export class NewEditorComponent implements OnInit, OnDestroy {
   }
 
   handleDrag(event: DragEvent) {
+    event.preventDefault();
     const isMedia = event.dataTransfer?.types.includes('Files')
     if (isMedia) {
       if (event.type === 'dragenter') {
